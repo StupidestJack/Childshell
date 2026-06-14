bc78eabbc17d7db95963693ee277d5e9f8b7e1ec
Niugnep <stupidestjack@gmail.com>
2026-06-14 10:57:44 +0800

    init: 自己掰開？（指原始碼）

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChildShell
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        /// 
+
+        public static string shellversion = "3.0";
+        public static int shellbuild = 14;
+        public static string progress = "Release";
+        [STAThread]        
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Looper());
+        }
+    }
+}
